8d6ef66d8eed3667b834d18f88ed4651bdfa3403
Andrew Vos <andrew.vos@gmail.com>
2013-12-17 00:16:59 +0000

    escape > and < in build output

--- public/scripts/build_output.js
+++ public/scripts/build_output.js
@@ -15,7 +15,8 @@ $(document).ready(function() {
 
 function update() {
   $.getJSON("/build_output_raw?id=" + $("#build_id").val(), function(data) {
-    var txt  = "\n\n\033[1;33;40m 33;40  \033[1;33;41m 33;41  \033[1;33;42m 33;42  \033[1;33;43m 33;43  \033[1;33;44m 33;44  \033[1;33;45m 33;45  \033[1;33;46m 33;46  \033[1m\033[0\n\n\033[1;33;42m >> Tests OK\n\n"
+    data.output = data.output.replace(">", "&gt;");
+    data.output = data.output.replace("<", "&lt;");
     var html = ansi_up.ansi_to_html(data.output);
     document.getElementById("output").innerHTML = html;
     if (window.autoScroll) {
