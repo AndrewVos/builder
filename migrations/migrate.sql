3aab817b6bc95d5792d92c8c7d97d9bcb4525f56
Andrew Vos <andrew.vos@gmail.com>
2014-01-03 00:00:30 +0000

    typo

--- migrations/migrate.sql
+++ migrations/migrate.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE builder_production
-
-CREATE TABLE github_builds (
+CREATE TABLE github_builds(
   access_token VARCHAR(100) primary key,
   owner VARCHAR(100) NOT NULL,
   repository VARCHAR(100) NOT NULL
