2debfe015fd24810c2ebcd3a842714e38e037bb8
Andrew Vos <andrew.vos@gmail.com>
2014-01-04 00:25:53 +0000

    use new field names in JS
    field names changed because jet does weird things with uppercase
    characters (likely converts them to snake case)

--- public/scripts/build.js
+++ public/scripts/build.js
@@ -8,33 +8,33 @@ function update() {
     $.each(data, function(i, build) {
       var container = $("#builds");
 
-      if ($("#"+build.ID).length == 0) {
+      if ($("#"+build.Id).length == 0) {
         var commits = "";
 
         if (build.Commits != null && build.Commits.length > 0) {
           for (i = 0; i < build.Commits.length; i++) {
             var commit = build.Commits[i];
             commits += "<div>";
-            commits += '<span class="label label-info">' + commit.SHA.slice(0,7) + '</span>';
+            commits += '<span class="label label-info">' + commit.Sha.slice(0,7) + '</span>';
             commits += "<span> " + commit.Message + "</span>";
             commits += "</div>";
           }
         }
 
-        var html = "<div id='"+build.ID+"' class='build'>" +
+        var html = "<div id='"+build.Id+"' class='build'>" +
           "<h2>" +
             "<div class='ball-container'><div class='ball'></div></div>" +
-            "<a href='/build_output?id=" + build.ID + "'>" +
-              build.Repo + "/" + build.Ref +
+            "<a href='/build_output?id=" + build.Id + "'>" +
+              build.Repository + "/" + build.Ref +
             "</a>" +
           "</h2>" +
             commits +
-            "<div><a href='" + build.GithubURL + "'>View on Github</a></div>" +
+            "<div><a href='" + build.GithubUrl + "'>View on Github</a></div>" +
         "</div>";
 
         container.prepend($(html));
       }
-      var buildLine = $("#" + build.ID);
+      var buildLine = $("#" + build.Id);
       if (build.Complete == true) {
         buildLine.removeClass("blue");
         if (build.Success == true) {
